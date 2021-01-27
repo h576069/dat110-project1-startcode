1171060fa246b15bb8c787a767190070a2bfa888
vilde <vilde@158.37.226.149>
2021-01-27 14:16:43 +0100

    messagClient.connect()

--- dat110-project1/src/no/hvl/dat110/messaging/MessagingClient.java
+++ dat110-project1/src/no/hvl/dat110/messaging/MessagingClient.java
@@ -3,32 +3,42 @@ package no.hvl.dat110.messaging;
 import java.io.IOException;
 
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import no.hvl.dat110.TODO;
 
 public class MessagingClient {
 
 	private String server;
+	
 	private int port;
 
 	public MessagingClient(String server, int port) {
 		this.server = server;
 		this.port = port;
+		System.out.println("Server="+ server);
+		System.out.println("port="+ port);
 	}
 
 	// connect to messaging server
 	public Connection connect() {
 
-		Socket clientSocket;
-		Connection connection = null;
-
-		// TODO
-		// create TCP socket for client and connection
-		// create connection object
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
+		Socket clientSocket=null;
+		Connection connection=null;
+		try {
+			clientSocket = new Socket(server, port);
+			connection = new Connection(clientSocket);
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
 		}
+		
+
+		
+	
 
 		return connection;
 	}
