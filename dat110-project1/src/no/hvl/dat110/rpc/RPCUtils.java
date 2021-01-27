db79d30a0727130996d6bcf6d3fd0dece067acbc
AnneDrottning <AnneDrottning@158-37-234-36.hvl.no>
2021-01-27 15:56:06 +0100

    Gjorde RPCUtils.void metoden, startet på int

--- dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCUtils.java
@@ -6,11 +6,12 @@ import no.hvl.dat110.TODO;
 
 public class RPCUtils {
 
-	// Utility methods for marshalling and marshalling of parameters and return values
+	// Utility methods for marshalling and marshalling of parameters and return
+	// values
 	// in RPC request and RPC responses
-	// data bytearrays and return byte arrays is according to the 
+	// data bytearrays and return byte arrays is according to the
 	// RPC message syntax [rpcid,parameter/return value]
-	
+
 	public static byte[] marshallString(byte rpcid, String str) {
 
 		byte[] encoded;
@@ -39,21 +40,18 @@ public class RPCUtils {
 
 	public static byte[] marshallVoid(byte rpcid) {
 
-		byte[] encoded;
-
-		// TODO: marshall RPC identifier in case of void type
-
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+		// A: siden det er void sender vi kun rpcid-en
+		byte[] encoded = new byte[1];
+		encoded[0] = rpcid;
 
 		return encoded;
 
 	}
 
 	public static void unmarshallVoid(byte[] data) {
-
-		// TODO: unmarshall void type
+		// A: mottar kun en void, så pakker bare ut id-en
+		// A: Som vi ikke trenger å pakke ut?
+		byte rpcid = data[0];
 	}
 
 	public static byte[] marshallBoolean(byte rpcid, boolean b) {
@@ -78,14 +76,31 @@ public class RPCUtils {
 	}
 
 	public static byte[] marshallInteger(byte rpcid, int x) {
+		// A: integer representeres ved 4 bytes
+		byte[] encoded = new byte[1 + 4];
 
-		byte[] encoded;
+		// A: setter inn rpcid:
+		encoded[0] = rpcid;
+		byte b = ((Integer) x).byteValue();
 
+		byte[] result = new byte[4];
+
+		result[0] = (byte) (x >> 24);
+		result[1] = (byte) (x >> 16);
+		result[2] = (byte) (x >> 8);
+		result[3] = (byte) (x /* >> 0 */);
+
+		System.out.println("int = " + x);
+		System.out.println("int som byte = " + ((Integer) x).byteValue());
+		System.out.println("int igjen = " + ((Byte) b).intValue());
+		System.out.println("evt: " + result[0] + ", " + result[1] + ", " + result[2] + ", " + result[3] + ", ");
+		
+		System.out.println("tilbake: :(");
 		// TODO: marshall RPC identifier and string into byte array
 
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+//		if (true) {
+//			throw new UnsupportedOperationException(TODO.method());
+//		}
 
 		return encoded;
 	}
