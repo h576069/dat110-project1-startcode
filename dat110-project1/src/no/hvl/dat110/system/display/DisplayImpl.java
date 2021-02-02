c665ae3923af2e018e89d81fb0e7eabf1d701ffd
AnneDrottning <AnneDrottning@annes-macbook-pro-147.home>
2021-02-02 14:33:50 +0100

    Fikset displayImpl

--- dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
+++ dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
@@ -1,6 +1,5 @@
 package no.hvl.dat110.system.display;
 
-import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.RPCImpl;
 import no.hvl.dat110.rpc.RPCUtils;
 
@@ -12,16 +11,11 @@ public class DisplayImpl implements RPCImpl {
 	
 	public byte[] invoke(byte[] request) {
 		
-		byte[] reply;
-		byte rpcid;
+		String str = RPCUtils.unmarshallString(request);
+		write(str);
+		byte rpcid = request[0];
+		byte[] reply = RPCUtils.marshallVoid(rpcid);
 		
-		// TODO: 
-		// implement unmarshalling, call, and marshall for write RPC method
-		// look at how this is done int he SensorImpl for the read method
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
 		
 		return reply;
 	}
