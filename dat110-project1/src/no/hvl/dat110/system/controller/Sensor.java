43db0073d52506bcc44aa90d185b25c0c2a5ace9
theachristianslund <theachristianslund@localhost>
2021-02-02 14:49:08 +0100

    read metoden for sensor er done

--- dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
@@ -2,23 +2,27 @@ package no.hvl.dat110.system.controller;
 
 import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.*;
+import no.hvl.dat110.system.sensor.SensorImpl;
 
 public class Sensor extends RPCStub {
 
 	private byte RPCID = 1;
-	
+
 	public int read() {
-		
+
 		int temp;
-		
+
 		// TODO
 		// implement marshalling, call and unmarshalling for read RPC method
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
-		
+
+		byte[] request = RPCUtils.marshallVoid(RPCID);
+
+		SensorImpl sensor = new SensorImpl();
+		byte[] reply = sensor.invoke(request);
+
+		temp = RPCUtils.unmarshallInteger(reply);
+
 		return temp;
 	}
-	
+
 }
