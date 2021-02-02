a55db63f55d38060b3af6ab371e58c6e67728bb0
vilde <vilde@DESKTOP-GAJC1PP>
2021-02-02 15:03:05 +0100

    Display

--- dat110-project1/src/no/hvl/dat110/system/controller/Display.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Display.java
@@ -2,16 +2,17 @@ package no.hvl.dat110.system.controller;
 
 import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.*;
+import no.hvl.dat110.system.display.DisplayImpl;
 
 public class Display extends RPCStub {
 
 	private byte RPCID = 1;
 
 	public void write(String message) {
-
-		// TODO
-		// implement marshalling, call and unmarshalling for write RPC method
-
-		throw new UnsupportedOperationException(TODO.method());
+		DisplayImpl dis= new DisplayImpl();
+		byte[] svar= dis.invoke(RPCUtils.marshallString(this.RPCID, message));
+		RPCUtils.unmarshallVoid(svar);
+  
+		
 	}
 }
